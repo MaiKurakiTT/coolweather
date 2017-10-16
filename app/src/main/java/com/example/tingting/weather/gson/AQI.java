1477ba3a564be730772ad0fc907fc17d3f927929
Tina <1040237162@qq.com>
2017-10-16 22:55:53 +0800

    commit。。

---
+++ app/src/main/java/com/example/tingting/weather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.tingting.weather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
